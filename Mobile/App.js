6f170eca7f2be0dadf08e52c48fc0a01c944b8fc
Adrian <135287367+Adriantech000@users.noreply.github.com>
2025-09-09 21:18:42 -0300

    mudnaça na paginha home

--- Mobile/App.js
+++ Mobile/App.js
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Home from './src/pages/home';
+import Paciente from './src/pages/cadastroPacient';
 
 export default function App() {
   return (
     
     <View style={styles.container}>
-      <Home />
+      <Paciente />
       <StatusBar style="auto" />
     </View>
   );
